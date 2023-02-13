54f42ee8b4aa21c000622d421bc2ea46849bf8b4
designersohan <sohanbd.sml@gmail.com>
2023-02-13 23:02:43 +0600

    init

--- asignment_Module3.php
+++ asignment_Module3.php
@@ -1,5 +1,7 @@
 <?php
 
+// The solution for the question no. 1
+
 function checkEvenOrOdd($number){
     if ($number % 2 == 0){
         echo "This number is Even";
@@ -11,3 +13,12 @@ function checkEvenOrOdd($number){
 
 $number = 8;
 echo checkEvenOrOdd($number);
+
+// The solution for the question no. 2
+
+$sum = 0;
+
+for ($nmuber = 1; $number <= 100; $number++) {
+    $sum += $number;
+}
+echo "The sum of the series is: $sum";
